3d3579a6b7ea156816a55608028cbac8fbe5cc7b
wassim echi <wassimechi@gmail.com>
2020-06-07 14:48:37 +0100

    layout

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,31 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule, } from '@angular/common';
-import { BrowserModule  } from '@angular/platform-browser';
-import { Routes, RouterModule } from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 
 import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
 
-const routes: Routes =[
+const routes: Routes = [
   {
-    path: '',
-    redirectTo: 'dashboard',
-    pathMatch: 'full',
-  }, {
-    path: '',
-    component: AdminLayoutComponent,
-    children: [{
-      path: '',
-      loadChildren: './layouts/admin-layout/admin-layout.module#AdminLayoutModule'
-    }]
-  }
+    path: 'artisanat',
+    loadChildren: () => import('./artisanat/artisanat.module').then(rm => rm.ArtisanatModule)
+  },
+  /*{ path: 'admin', component: AdminLayoutComponent },
+  { path: '**', redirectTo: 'admin' }*/
 ];
 
 @NgModule({
   imports: [
     CommonModule,
     BrowserModule,
-    RouterModule.forRoot(routes,{
-       useHash: true
+    RouterModule.forRoot(routes, {
+      preloadingStrategy: PreloadAllModules
     })
   ],
   exports: [
