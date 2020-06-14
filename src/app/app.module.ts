4fac129dd93cdf28c69b68dcf7ab3fd86fc895fe
wassim echi <wassimechi@gmail.com>
2020-06-14 20:12:51 +0100

    shop / products

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
 
@@ -9,15 +10,6 @@ import { AppRoutingModule } from './app.routing';
 import { ComponentsModule } from './components/components.module';
 
 import { AppComponent } from './app.component';
-
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { UserProfileComponent } from './user-profile/user-profile.component';
-import { TableListComponent } from './table-list/table-list.component';
-import { TypographyComponent } from './typography/typography.component';
-import { IconsComponent } from './icons/icons.component';
-import { MapsComponent } from './maps/maps.component';
-import { NotificationsComponent } from './notifications/notifications.component';
-import { UpgradeComponent } from './upgrade/upgrade.component';
 import {
   AgmCoreModule
 } from '@agm/core';
@@ -32,8 +24,9 @@ import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.compon
     ComponentsModule,
     RouterModule,
     AppRoutingModule,
+    HttpClientModule,
     AgmCoreModule.forRoot({
-      apiKey: 'YOUR_GOOGLE_MAPS_API_KEY'
+      apiKey: 'AIzaSyDQvX7Zv9GqP4abRRHmqiClQP-0TzMCedk'
     })
   ],
   declarations: [
--- src/app/artisanat/artisanat.module.ts
+++ src/app/artisanat/artisanat.module.ts
@@ -10,6 +10,8 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatSliderModule } from '@angular/material/slider';
+
 import { NewShopComponent } from './new-shop/new-shop.component';
 import { MyShopComponent } from './my-shop/my-shop.component';
 import { MatTableModule } from '@angular/material/table';
@@ -18,9 +20,12 @@ import { WorkersComponent } from './my-shop/workers/workers.component';
 import { SmallCardComponent } from './my-shop/small-card/small-card.component';
 import { MyProductsComponent } from './my-shop/my-products/my-products.component';
 import { MyGridComponent } from './my-shop/my-grid/my-grid.component';
+import { ProductCardComponent } from './products/product-card/product-card.component';
+import { MyCartComponent } from './my-cart/my-cart.component';
+import { FrontShopComponent } from './front-shop/front-shop.component';
 
 @NgModule({
-  declarations: [ProductsComponent, NewProductComponent, NewShopComponent, MyShopComponent, WorkersComponent, SmallCardComponent, MyProductsComponent, MyGridComponent],
+  declarations: [ProductsComponent, NewProductComponent, NewShopComponent, MyShopComponent, WorkersComponent, SmallCardComponent, MyProductsComponent, MyGridComponent, ProductCardComponent, MyCartComponent, FrontShopComponent],
   imports: [
     CommonModule,
     ArtisanatRoutingModule,
@@ -31,7 +36,8 @@ import { MyGridComponent } from './my-shop/my-grid/my-grid.component';
     MatSelectModule,
     MatTooltipModule,
     MatTableModule,
-    MatListModule
+    MatListModule,
+    MatSliderModule
   ]
 })
 export class ArtisanatModule { }
